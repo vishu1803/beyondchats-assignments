524944af9b1a451a7b72ada85912c3a82940ffd3
vishu1803 <97516061+vishu1803@users.noreply.github.com>
2025-12-25 18:14:49 +0530

    fixing deployment bugs

--- backend-laravel/backend-laravel/routes/api.php
+++ backend-laravel/backend-laravel/routes/api.php
@@ -1,8 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\DB;
 use App\Http\Controllers\Api\ArticleController;
 
 Route::apiResource('articles', ArticleController::class);
 Route::get('articles-latest', [ArticleController::class, 'latest']);
 Route::post('/scrape', [\App\Http\Controllers\ScrapeController::class, 'scrape']);
+Route::get('/debug-db', function () {
+    return DB::select('select now()');
+});
